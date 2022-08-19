5542f9ab789a3152ed8faf5d4ac0ff5f8d942f10
OSERHIRDEV <oserhir10@gmail.com>
2022-08-19 21:40:37 +0100

    get Quote Data

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
- 
+let getQuote = document.querySelector("button");
+let Quote = document.getElementById("quote");
+let Author = document.getElementById("author");
+
+getQuote.addEventListener("click", () => {
+  getQuoteData()
+    .then((res) => {
+      showQuoteData(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+getQuoteData = () => {
+  const quoteUrl = fetch("https://api.quotable.io/random");
+  return quoteUrl
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+showQuoteData = (data) => {
+  Quote.innerHTML = `${data["content"]}`;
+  Author.innerHTML = `${data["author"]}`;
+
+  console.log();
+};
